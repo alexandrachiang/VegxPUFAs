1ff62aecef7a20a3c6b0f3d145fb4b34d45ac393
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-09-12 00:35:35 -0400

    Update CountVeg.R

--- CountVeg.R
+++ CountVeg.R
@@ -9,23 +9,16 @@ suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
 suppressMessages(library(tidyverse))
 suppressMessages(library(ggpubr))
-suppressMessages(library(RNOmni))
+suppressMessages(library(ukbtools))
+suppressMessages(library(rio))
 
 setwd("/scratch/ahc87874/Fall2022")
 
-source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
+#source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
+
+ukb <- ukb_df("ukb34137")
+ukb <- as_tibble(ukb)
+
 withdrawn<-read.csv("w48818_20210809.csv", header = FALSE)
-
-bd <- as_tibble(bd)
-
-pheno<-bd%>%select(f.eid, f.21003.0.0, f.31.0.0, 
-                   f.189.0.0,
-                   f.54.0.0, f.22000.0.0
-                    )
-
-colnames(pheno)<-c("IID", "Age", "Sex",  
-                   "Townsend",
-                   "Assessment_center", "Geno_batch"
-                    )
 
 pheno <- pheno[!(pheno$IID %in% withdrawn$V1), ]
