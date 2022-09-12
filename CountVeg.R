ca74c0496239015b077eb7d416e7566631b43ef2
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-09-11 22:01:09 -0400

    Update CountVeg.R

--- CountVeg.R
+++ CountVeg.R
@@ -16,6 +16,8 @@ setwd("/scratch/ahc87874/Fall2022")
 source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
 withdrawn<-read.csv("w48818_20210809.csv", header = FALSE)
 
+bd <- as_tibble(bd)
+
 pheno<-bd%>%select(f.eid, f.21003.0.0, f.31.0.0, 
                    f.189.0.0,
                    f.54.0.0, f.22000.0.0
