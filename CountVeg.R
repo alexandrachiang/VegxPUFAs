939ef9619290b5804c167f794925de17468e1fd7
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-09-11 21:42:28 -0400

    Update CountVeg.R

--- CountVeg.R
+++ CountVeg.R
@@ -16,9 +16,6 @@ setwd("/scratch/ahc87874/Fall2022")
 source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
 withdrawn<-read.csv("w48818_20210809.csv", header = FALSE)
 
-pheno <- pheno[!(pheno$IID %in% withdrawn$V1), ]
-
-
 pheno<-bd%>%select(f.eid, f.21003.0.0, f.31.0.0, 
                    f.189.0.0,
                    f.54.0.0, f.22000.0.0
@@ -28,3 +25,5 @@ colnames(pheno)<-c("IID", "Age", "Sex",
                    "Townsend",
                    "Assessment_center", "Geno_batch"
                     )
+
+pheno <- pheno[!(pheno$IID %in% withdrawn$V1), ]
