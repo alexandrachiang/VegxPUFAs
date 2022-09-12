53d515afeea36fc30189f921defeb08a343c0c1b
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-09-12 02:02:28 -0400

    Update CountVeg.R

--- CountVeg.R
+++ CountVeg.R
@@ -21,10 +21,30 @@ ukb <- as_tibble(ukb)
 
 withdrawn <-read.csv("w48818_20210809.csv", header = FALSE)
 
+#invitation_to_complete_online_24hour_recall_dietary_questionnaire_acceptance
+
 #Get all participants that self-IDed as vegetarian/vegan at least once in the initial and recall surveys
 ukbveg <- ukb %>% filter(if_any(starts_with("type_of_special_diet_followed"), ~ . %in% c("Vegetarian", "Vegan")))
+#9454 rows
 
-apply(ukbveg2, 2, table)
+x <- ukb %>% select((starts_with(c("invitation_to_complete_online_24hour_recall_dietary_questionnaire_acceptance", "type_of_special_diet_followed"))))                   
+#apply(x, 2, table)
 
+#How to check for initial?
+for (i in 1:4) {
+  rec <- paste("invitation_to_complete_online_24hour_recall_dietary_questionnaire_acceptance_f110001_", i, "_0", sep = "")
+  varname <- paste("total", i, sep="_")
+  x[[varname]] <- NA
+  
+  #new column for completed = "nonveg", else = NA
+  x[[varname]][x[, rec] == "Completed"] <- "Nonveg"
+  
+  for (j in 0:5) {
+    inst <- paste("type_of_special_diet_followed_f20086_", i, "_", j, sep = "")
+    x[[total]][x[, inst] == "Vegetarian" | x[, inst] == "Vegan"] <- "Veg"
+  }
+}
 
-pheno <- pheno[!(pheno$IID %in% withdrawn$V1), ]
+#add up to find average of veg responses
+
+#pheno <- pheno[!(pheno$IID %in% withdrawn$V1), ]
