d2b32ab20bf5efba146a43dacc31329b350fd64a
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-09-21 12:01:37 -0400

    Update CountVeg.R

--- CountVeg.R
+++ CountVeg.R
@@ -2,43 +2,60 @@
 # CSRV = self-ID vegatarian, field 20086
 # SSRV = true vegetarian, field 
 
-#Remove withdrawn at the end
-
+#Load packages and set up wd
 suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
 suppressMessages(library(tidyverse))
 suppressMessages(library(ggpubr))
-suppressMessages(library(ukbtools))
+suppressMessages(library(ukbtools)) #<3
 suppressMessages(library(rio))
 
 setwd("/scratch/ahc87874/Fall2022")
-
 #source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
 
+#Load dataset
 ukb <- ukb_df("ukb34137")
 ukb <- as_tibble(ukb)
 
+#Remove withdrawn participants from dataset
 withdrawn <-read.csv("w48818_20210809.csv", header = FALSE)
+ukb <- ukb[!(ukb$eid %in% withdrawn$V1), ]
 
-select(f.eid, f.31.0.0, f.22001.0.0, f.21000.0.0,
+select(f.eid, f.21003.0.0, f.31.0.0, f.22001.0.0, f.21000.0.0,
                       f.22027.0.0, f.22019.0.0,
-                      f.22021.0.0)
+                      f.22021.0.0, f.189.0.0,
+                      f.54.0.0, f.22000.0.0)
 
-colnames(bd_QC)<-c("IID", "Sex", "Genetic_Sex", "Race",
+colnames(bd_QC)<-c("IID", "Age", "Sex", "Genetic_Sex", 
+                   
+                   "Race",
                    "Outliers_for_het_or_missing", "SexchrAneuploidy",
-                   "Genetic_kinship")
+                   "Genetic_kinship", "Townsend",
+                   "Assessment_center", "Geno_batch")
 
 #Select necessary columns
-#Need to add covar, SSRV, and pheno columns
-ukb2 <- ukb %>% select(eid, starts_with(c("dayofweek_questionnaire_completed", "type_of_special_diet_followed", "daily_dietary_data_credible")))
+#Need to add BMI, SSRV, and pheno columns
+ukb2 <- ukb %>% select(eid, age_when_attended_assessment_centre_f21003_0_0, sex_f31_0_0, genetic_sex_f22001_0_0, 
+                       ethnic_background_f21000_0_0, outliers_for_heterozygosity_or_missing_rate_f22027_0_0, 
+                       sex_chromosome_aneuploidy_f22019_0_0, genetic_kinship_to_other_participants_f22021_0_0, 
+                       townsend_deprivation_index_at_recruitment_f189_0_0, uk_biobank_assessment_centre_f54_0_0, 
+                       genotype_measurement_batch_f22000_0_0, 
+                       starts_with(c("dayofweek_questionnaire_completed", "type_of_special_diet_followed", "daily_dietary_data_credible")))
 #apply(ukb2, 2, table)
-#41 columns
+# columns
+
+#Add Age^2 column
+ukb2 <- ukb2 %>% mutate(age_when_attended_assessment_centre_squared = age_when_attended_assessment_centre_f21003_0_0^2) %>% 
+                 select(eid, starts_with("age_when_attended_assessment_centre"), everything())
 
 #Remove participants that never answered 20086/never did a dietary survey
 ukb3 <- ukb2[rowSums(is.na(ukb2[, paste("dayofweek_questionnaire_completed_f20080", 0:4, "0", sep = "_")])) != 5,]
 #nrow(ukb3)
 #211018 rows
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------
+#Pheno QC
+#idk
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 #CSRV
 
@@ -72,15 +89,15 @@ for (i in 0:4) { #instance
 #print(n = 50, ukb3[36:41])
 
 table(ukbCSRV$CSRV)
-#204172 CSRV NonVeg
-#6846 CSRV Veg
-#Michael had 5733
+#204172 CSRV NonVeg pre-QC
+#6846 CSRV Veg pre-QC
+#Michael had 5733 post-QC
 
 ukbCSRV %>% select(starts_with("is_vegetarian")) %>% filter_all(all_vars(!is.na(.))) 
 #or ukb3 %>% select(starts_with("is_vegetarian")) %>% filter(if_all(everything(), ~ grepl("", .)))
 ukbCSRV %>% select(starts_with("is_vegetarian")) %>% filter_all(all_vars(. == "Veg"))
-#5766 answered all surveys
-#182 answered Veg across all surveys
+#5766 answered all surveys pre-QC
+#182 answered Veg across all surveys pre-QC
 
 #names(ukb3)
 # [1] "eid"
@@ -103,6 +120,6 @@ idk2 <- idk[rowSums(!is.na(idk[, paste("daily_dietary_data_credible_f100026", 0:
 #Withdrawn
 #pheno <- pheno[!(pheno$IID %in% withdrawn$V1), ]
 ukbCSRVwithdrawn <- ukbCSRV[!(ukbCSRV$eid %in% withdrawn$V1), ]
-table(ukbCSRVwithdrawn$CSRV)
+table(ukbCSRVwithdrawn$CSRV) #OLD
 #204140 CSRV NonVeg (-32)
 #6844 CSRV Veg (-2)
