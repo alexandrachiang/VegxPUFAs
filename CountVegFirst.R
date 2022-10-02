ffa227a4cf3192957b1a31e10a0050aef48bb041
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-10-02 16:15:13 -0400

    Update CountVegFirst.R

--- CountVegFirst.R
+++ CountVegFirst.R
@@ -11,9 +11,9 @@ suppressMessages(library(ukbtools)) #<3
 suppressMessages(library(rio))
 
 setwd("/scratch/ahc87874/Fall2022")
-#source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
 
 #Load dataset
+#source('/scratch/ahc87874/Fall2022/load_UKBphenotables.R')
 ukb <- ukb_df("ukb34137")
 #ukb <- import("ukb34137.tsv")
 ukb <- as_tibble(ukb)
