7145c81a0f40aa43f2d25b71db3273654e03c645
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-10-24 10:53:38 -0400

    Update Compare.R

--- Compare.R
+++ Compare.R
@@ -1,3 +1,15 @@
 #Compare our numbers with Michael's
 
-source()
+#Load packages and set up wd
+suppressMessages(library(plyr))
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyverse))
+#suppressMessages(library(ggpubr))
+suppressMessages(library(ukbtools)) #<3
+suppressMessages(library(rio))
+
+source('/scratch/ahc87874/Fall2022/pheno/load_UKBphenotables.R')
+ukbnames <- read.csv("ukbnames.csv")
+
+names(bd) <- ukbnames
+
