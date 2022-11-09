920fdc72d3e3aaa336c4bee90f315f22d2b08e5d
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-08 19:27:48 -0500

    Update qqman.R

--- qqman.R
+++ qqman.R
@@ -8,13 +8,13 @@ exposures <- c("CSRV", "SSRV")
 
 for (i in phenos) {
   GEMdir <- "/scratch/ahc87874/Fall2022/GEM"
-  print(i)
+  print(paste("pheno:", i))
   
   for (j in exposures) {
-    print(j)
+    print(paste("exposure:", j))
     if (TRUE) { #Combine GEM output for pheno and exposure from chr 1-22 into one data frame
       for (k in 1:22) {
-        print(i)
+        print(paste("chr:", k))
         infile <- as_tibble(read.table(paste(GEMdir, i, paste(i, "x", j, "-chr", k, sep = ""), sep = "/"), 
                                        header = TRUE, stringsAsFactors = FALSE))
 
