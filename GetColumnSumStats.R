f8449befd90fc5b0c01df4f38c0fb047141dc43b
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-18 12:02:24 -0500

    Update GetColumnSumStats.R

--- GetColumnSumStats.R
+++ GetColumnSumStats.R
@@ -1,2 +1,11 @@
 SumStats <- function(df) {
+  means <- as.data.frame(colMeans(select_if(df, is.numeric), na.rm = TRUE))
+  NAs <- as.data.frame(colSums(is.na(df)))
+  
+  print(nrow(means) == nrow(NAs))
+  
+  sumstats <- as_tibble(data.frame(pheno = row.names(means), means, NAs))
+  names(sumstats) <- c("phenos", "means", "NAs")
+  
+  print(sumstats)
 }
