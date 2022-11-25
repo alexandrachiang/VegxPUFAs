98a630ce219df2ebb7951b55ffa67ee26066c354
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-25 13:00:01 -0500

    Create qqman.sh

---
+++ qqman.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=qqman
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=20:00:00
+#SBATCH --mem=1000
+#SBATCH --output=qqman.%j.out
+#SBATCH --error=qqman.%j.err
+#SBATCH --mail-user=ahc87874@uga.edu
+#SBATCH --mail-type=ALL
+
+cd $SLURM_SUBMIT_DIR
+
+module load R/4.1.3-foss-2020b
+R CMD BATCH qqman.R
