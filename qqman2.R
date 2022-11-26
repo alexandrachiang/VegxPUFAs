d5ce2e3b881125813c23823734600d16413190f5
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-26 13:46:35 -0500

    Update qqman2.R

--- qqman2.R
+++ qqman2.R
@@ -6,37 +6,37 @@ source("ManhattanCex.R")
 
 setwd("/scratch/ahc87874/Fall2022/")
 
-suffix <- ""
-#"" "woCred" "wKeep"
+allsuffix <- c("", "woCred", "wKeep")
 
-phenos <- c("w3FA_NMR", "w3FA_NMR_TFAP", "w6FA_NMR", "w6FA_NMR_TFAP", "w6_w3_ratio_NMR", "DHA_NMR", 
-	    "DHA_NMR_TFAP", "LA_NMR", "LA_NMR_TFAP", "PUFA_NMR", "PUFA_NMR_TFAP", "MUFA_NMR", 
-	    "MUFA_NMR_TFAP", "PUFA_MUFA_ratio_NMR")
+for (suffix in allsuffix) {
+	phenos <- c("w3FA_NMR", "w3FA_NMR_TFAP", "w6FA_NMR", "w6FA_NMR_TFAP", "w6_w3_ratio_NMR", "DHA_NMR", 
+		    "DHA_NMR_TFAP", "LA_NMR", "LA_NMR_TFAP", "PUFA_NMR", "PUFA_NMR_TFAP", "MUFA_NMR", 
+		    "MUFA_NMR_TFAP", "PUFA_MUFA_ratio_NMR")
 
-if (suffix == "") {
-  exposures <- c("CSRV", "SSRV")
-} else {
-  exposures <- c("SSRV")
-}
+	if (suffix == "") {
+	  exposures <- c("CSRV", "SSRV")
+	} else {
+	  exposures <- c("SSRV")
+	}
 
-for (i in phenos) {
-  GEMdir <- paste("/scratch/ahc87874/Fall2022/GEM", suffix, sep = "")
-  
-  print(paste("pheno:", i))
-  
-  for (j in exposures) {
-    print(paste("exposure:", j))
-    infileall <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "x", j, suffix, "all.txt", sep = ""), 
-                                      header = TRUE, stringsAsFactors = FALSE))
-    
-    print("QQ")
-    #Make qq plot
-    outdirqq = "/scratch/ahc87874/Fall2022/qqplots/"
-    png(filename = paste(outdirqq, i, "x", j, suffix, "qq.png", sep = ""), type = "cairo", 
-        width = 600, height = 600)
-    qq(infileall$P, main = paste("Q-Q plot of ", i, "x", j, " GWIS p-values", sep = ""))
-    dev.off()
-  } #j exposures
-} #i phenos
+	for (i in phenos) {
+	  GEMdir <- paste("/scratch/ahc87874/Fall2022/GEM", suffix, sep = "")
 
+	  print(paste("pheno:", i))
+
+	  for (j in exposures) {
+	    print(paste("exposure:", j))
+	    infileall <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "x", j, suffix, "all.txt", sep = ""), 
+					                   header = TRUE, stringsAsFactors = FALSE))
+
+	    print("QQ")
+	    #Make qq plot
+	    outdirqq = "/scratch/ahc87874/Fall2022/qqplots/"
+	    png(filename = paste(outdirqq, i, "x", j, suffix, "qq.png", sep = ""), type = "cairo", 
+          width = 600, height = 600)
+	    qq(infileall$P, main = paste("Q-Q plot of ", i, "x", j, " GWIS p-values", sep = ""))
+	    dev.off()
+	  } #j exposures
+	} #i phenos
+} 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
