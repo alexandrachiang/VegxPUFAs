dc18f14bf12c518a831dcf87c03fb05710ceb0ab
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-26 13:43:26 -0500

    Update qqman.R

--- qqman.R
+++ qqman.R
@@ -93,9 +93,9 @@ for (i in phenos) {
     print("QQ")
     #Make qq plot
     outdirqq = "/scratch/ahc87874/Fall2022/qqplots/"
-    png(filename = paste(outdirqq, i, "x", j, "qq.png", sep = ""), type = "cairo", 
+    png(filename = paste(outdirqq, i, "x", j, suffix, "qq.png", sep = ""), type = "cairo", 
         width = 600, height = 600)
-    qq(infileall$P, main = paste("Q-Q plot of ", i, "x", j, suffix, " GWIS p-values", sep = ""))
+    qq(infileall$P, main = paste("Q-Q plot of ", i, "x", j, " GWIS p-values", sep = ""))
     dev.off()
   } #j exposures
 } #i phenos
