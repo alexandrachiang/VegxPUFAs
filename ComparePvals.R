8af50ad73f0f8709e9e2a3557c642e5915519e8a
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-27 12:49:54 -0500

    Update ComparePvals.R

--- ComparePvals.R
+++ ComparePvals.R
@@ -1,1 +1,38 @@
+library(tidyverse)
 
+setwd("/scratch/ahc87874/Fall2022/")
+
+suffix <- "woCred"
+#"" "woCred" "wKeep"
+
+phenos <- c("w3FA_NMR", "w3FA_NMR_TFAP", "w6FA_NMR", "w6FA_NMR_TFAP", "w6_w3_ratio_NMR", "DHA_NMR", 
+	    "DHA_NMR_TFAP", "LA_NMR", "LA_NMR_TFAP", "PUFA_NMR", "PUFA_NMR_TFAP", "MUFA_NMR", 
+	    "MUFA_NMR_TFAP", "PUFA_MUFA_ratio_NMR")
+
+#add for i in phenos
+
+i <- "w3FA_NMR_TFAP"
+
+CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xCSRV", "all.txt", sep = ""), 
+                                        header = TRUE, stringsAsFactors = FALSE))
+SSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xSSRV", suffix, "all.txt", sep = ""), 
+                                        header = TRUE, stringsAsFactors = FALSE))
+
+CSRV <- CSRV %>% mutate(CSRVP = -log10(P))
+SSRV <- SSRV %>% mutate(SSRVP = -log10(P))
+
+Both <- inner_join(CSRV, SSRV, by = c("SNP", "CHR")) %>% arrange(desc(SSRVP))
+
+outdirqq = "/scratch/ahc87874/Fall2022/Pvalplots/"
+png(filename = paste(outdirqq, "ComparePvals", i, suffix, ".png", sep = ""), type = "cairo", 
+    width = 600, height = 600)
+
+ggplot(Both) + 
+  geom_point(aes(x = CSRVP, y = SSRVP), alpha = 0.2) +
+  geom_hline(yintercept = -log10(5e-08), linetype = "dashed", color = "red") +
+  geom_vline(xintercept = -log10(5e-08), linetype = "dashed", color = "red") + 
+  labs(title = paste("Compare p-values of ", i, sep = ""), 
+       xlab = "CSRV",
+       ylab = "SSRV") 
+
+dev.off()
