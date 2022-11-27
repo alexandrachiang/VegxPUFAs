2626a36f863991bc5c56d792006c7132b1060f05
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-27 14:35:00 -0500

    Update ComparePvals.R

--- ComparePvals.R
+++ ComparePvals.R
@@ -111,6 +111,7 @@ ggplot(Both) +
        y = "SSRV") 
 dev.off()
 
+
 suffix <- "wKeep"
 i <- "w3FA_NMR_TFAP"
 CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xCSRV", "all.txt", sep = ""), 
@@ -177,6 +178,77 @@ ggplot(Both) +
   geom_hline(yintercept = -log10(5e-08), linetype = "dashed", color = "red") +
   geom_vline(xintercept = -log10(5e-08), linetype = "dashed", color = "red") + 
   labs(title = paste("Compare p-values of ", i, " ", suffix, sep = ""), 
+       x = "CSRV",
+       y = "SSRV") 
+dev.off()
+
+
+suffix <- ""
+i <- "w3FA_NMR_TFAP"
+CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xCSRV", "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+SSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xSSRV", suffix, "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+
+CSRV <- CSRV %>% mutate(CSRVP = -log10(P))
+SSRV <- SSRV %>% mutate(SSRVP = -log10(P))
+
+Both <- inner_join(CSRV, SSRV, by = c("SNP", "CHR")) %>% arrange(desc(SSRVP))
+
+outdir = "/scratch/ahc87874/Fall2022/Pvalplots/"
+png(filename = paste(outdir, "ComparePvals_", i, suffix, ".png", sep = ""), type = "cairo", 
+    width = 600, height = 600)
+ggplot(Both) + 
+	geom_point(aes(x = CSRVP, y = SSRVP), alpha = 0.1) +
+  geom_hline(yintercept = -log10(5e-08), linetype = "dashed", color = "red") +
+  geom_vline(xintercept = -log10(5e-08), linetype = "dashed", color = "red") + 
+  labs(title = paste("Compare p-values of ", i, sep = ""), 
+       x = "CSRV",
+       y = "SSRV") 
+dev.off()
+
+i <- "w6_w3_ratio_NMR"
+CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xCSRV", "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+SSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xSSRV", suffix, "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+
+CSRV <- CSRV %>% mutate(CSRVP = -log10(P))
+SSRV <- SSRV %>% mutate(SSRVP = -log10(P))
+
+Both <- inner_join(CSRV, SSRV, by = c("SNP", "CHR")) %>% arrange(desc(SSRVP))
+
+outdir = "/scratch/ahc87874/Fall2022/Pvalplots/"
+png(filename = paste(outdir, "ComparePvals_", i, suffix, ".png", sep = ""), type = "cairo", 
+    width = 600, height = 600)
+ggplot(Both) + 
+	geom_point(aes(x = CSRVP, y = SSRVP), alpha = 0.1) +
+  geom_hline(yintercept = -log10(5e-08), linetype = "dashed", color = "red") +
+  geom_vline(xintercept = -log10(5e-08), linetype = "dashed", color = "red") + 
+  labs(title = paste("Compare p-values of ", i, sep = ""), 
+       x = "CSRV",
+       y = "SSRV") 
+dev.off()
+
+i <- "LA_NMR_TFAP"
+CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xCSRV", "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+SSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xSSRV", suffix, "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+
+CSRV <- CSRV %>% mutate(CSRVP = -log10(P))
+SSRV <- SSRV %>% mutate(SSRVP = -log10(P))
+
+Both <- inner_join(CSRV, SSRV, by = c("SNP", "CHR")) %>% arrange(desc(SSRVP))
+
+outdir = "/scratch/ahc87874/Fall2022/Pvalplots/"
+png(filename = paste(outdir, "ComparePvals_", i, suffix, ".png", sep = ""), type = "cairo", 
+    width = 600, height = 600)
+ggplot(Both) + 
+	geom_point(aes(x = CSRVP, y = SSRVP), alpha = 0.1) +
+  geom_hline(yintercept = -log10(5e-08), linetype = "dashed", color = "red") +
+  geom_vline(xintercept = -log10(5e-08), linetype = "dashed", color = "red") + 
+  labs(title = paste("Compare p-values of ", i, sep = ""), 
        x = "CSRV",
        y = "SSRV") 
 dev.off()
