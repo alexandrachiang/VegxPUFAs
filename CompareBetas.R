5f3f178db84e12f2d0ed41aa0cd2eafea4152132
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-27 15:04:09 -0500

    Update CompareBetas.R

--- CompareBetas.R
+++ CompareBetas.R
@@ -10,48 +10,47 @@ allsuffix <- c("", "woCred", "wKeep")
 phenos <- c("w3FA_NMR_TFAP", "w6_w3_ratio_NMR", "LA_NMR_TFAP")
 
 for (suffix in allsuffix) {
-  if (suffix == "") {
-    exposures <- c("CSRV", "SSRV")
-  } else {
-    exposures <- c("SSRV")
-  }
-  
+  print(suffix)
   for (i in phenos) {
-    GEMdir <- paste("/scratch/ahc87874/Fall2022/GEM", suffix, sep = "")
+    print(i)
+    CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xCSRV", "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
+    SSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/", i, "xSSRV", suffix, "all.txt", sep = ""), 
+                                            header = TRUE, stringsAsFactors = FALSE))
 
-    print(paste("pheno:", i))
+    names(CSRV) <- c("CHR", "BP", "P", "CSRVBeta", "SNP")
+    names(SSRV) <- c("CHR", "BP", "P", "SSRVBeta", "SNP")
+    
+    CSRV <- CSRV %>% mutate(CSRVP = -log10(P))
+    SSRV <- SSRV %>% mutate(SSRVP = -log10(P))
 
-    for (j in exposures) {
-      print(paste("exposure:", j))
-      if (TRUE) { #Combine GEM output for pheno and exposure from chr 1-22 into one data frame
-        for (k in 1:22) {
-          print(paste("chr:", k))
-          infile <- as_tibble(read.table(paste(GEMdir, i, paste(i, "x", j, "-chr", k, sep = ""), sep = "/"), 
-                                         header = TRUE, stringsAsFactors = FALSE))
+    Both <- inner_join(CSRV, SSRV, by = c("SNP", "CHR")) %>% arrange(desc(SSRVP))
 
-          #Subset data
-          infilesub <- infile %>% select(CHR, POS, robust_P_Value_Interaction, starts_with("Beta_G"), RSID)
-
-          #Add to input
-          if (k == 1) {
-            infileall <- infilesub
-          } else {
-            infileall <- rbind(infileall, infilesub)
-          } #ifelse
-        } #k chr number
-
-        #Save data table of all chr for pheno x exposure
-        outdirFUMA = "/scratch/ahc87874/Fall2022/Combined/"
-        write.table(infileall, paste(outdirFUMA, i, "x", j, suffix, "all.txt", sep = ""), 
-                    row.names = FALSE, quote = FALSE)
-      } else {
-        infileall <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/Combined/", i, "x", j, suffix, "all.txt", sep = ""), 
-                                          header = TRUE, stringsAsFactors = FALSE))
-      } 
-    } #exposures
-  } #phenos
+    print("p-values")
+    pvalplot <- ggplot(Both) + 
+      geom_point(aes(x = CSRVP, y = SSRVP), alpha = 0.1) +
+      geom_hline(yintercept = -log10(5e-08), linetype = "dashed", color = "red") +
+      geom_vline(xintercept = -log10(5e-08), linetype = "dashed", color = "red") + 
+      labs(title = paste("Compare p-values of ", i, " ", suffix, sep = ""), 
+           x = "CSRV",
+           y = "SSRV") 
+	  
+    png(filename = paste("ComparePvals_", i, suffix, ".png", sep = ""), type = "cairo", width = 600, height = 600)
+    pvalplot
+    dev.off()
+    
+    print("betas")
+    betaplot <- ggplot(Both) + 
+      geom_point(aes(x = CSRVBeta, y = SSRVBeta), alpha = 0.1) +
+      labs(title = paste("Compare Betas of ", i, " ", suffix, sep = ""), 
+           x = "CSRV",
+           y = "SSRV") 
+	  
+    png(filename = paste("CompareBetas_", i, suffix, ".png", sep = ""), type = "cairo", width = 600, height = 600)
+    betaplot
+    dev.off()
+  } #i phenos
 } #suffix
-
 
 #-----------------------------------------------------------------------------------------------------------------
 #Loop doesnt work?
