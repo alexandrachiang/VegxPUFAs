76dfa4f9dfe800fd9df7d86d7676f1c8bbbadd55
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-30 11:01:54 -0500

    Update SigSNPsTable.R

--- SigSNPsTable.R
+++ SigSNPsTable.R
@@ -9,28 +9,41 @@ phenos <- c("w3FA_NMR", "w3FA_NMR_TFAP", "w6FA_NMR", "w6FA_NMR_TFAP", "w6_w3_rat
             
 suffix <- c("wKeep")
 
-SigSNPs <- as_tibble(cbind(rep(phenos, each = 2), 
+SNPsSum <- as_tibble(cbind(rep(phenos, each = 2), 
                                rep(c("CSRV", "SSRV"), times = length(phenos)), 
                                data.frame(matrix(ncol = 3, nrow = 2 * length(phenos)))))
-names(SigSNPs) <- c("Phenotype", "Exposure", "p1eneg5", "p5eneg8", "Smallestp")
+names(SNPsSum) <- c("Phenotype", "Exposure", "p1eneg5", "p5eneg8", "Smallestp")
+
+SigSNPs <- as_tibble(matrix(ncol = 7))
+names(SigSNPs) <- c("Pheno", "Exposure", "CHR", "BP", "P", "Beta", "SNP")
 
 for (i in 1:length(phenos)) {
   CSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/Combined/", phenos[i], "xCSRV", "all.txt", sep = ""), 
                                             header = TRUE, stringsAsFactors = FALSE))
   SSRV <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/Combined/", phenos[i], "xSSRV", suffix, "all.txt", sep = ""), 
                                             header = TRUE, stringsAsFactors = FALSE))
-  names(CSRV) <- c("CHR", "BP", "P", "CSRVBeta", "SNP")
-  names(SSRV) <- c("CHR", "BP", "P", "SSRVBeta", "SNP")
+  names(CSRV) <- c("CHR", "BP", "P", "Beta", "SNP")
+  names(SSRV) <- c("CHR", "BP", "P", "Beta", "SNP")
   
-  SigSNPs$p1eneg5[1 + 2 * (i - 1)] <- CSRV %>% filter(P <= 1e-5) %>% nrow()
-  SigSNPs$p1eneg5[2 + 2 * (i - 1)] <- SSRV %>% filter(P <= 1e-5) %>% nrow()
-  SigSNPs$p5eneg8[1 + 2 * (i - 1)] <- CSRV %>% filter(P <= 5e-8) %>% nrow()
-  SigSNPs$p5eneg8[2 + 2 * (i - 1)] <- SSRV %>% filter(P <= 5e-8) %>% nrow()
-  SigSNPs$Smallestp[1 + 2 * (i - 1)] <- scientific(min(CSRV$P, na.rm = TRUE), digits = 6)
-  SigSNPs$Smallestp[2 + 2 * (i - 1)] <- scientific(min(SSRV$P, na.rm = TRUE), digits = 6)
+  SNPsSum$p1eneg5[1 + 2 * (i - 1)] <- CSRV %>% filter(P <= 1e-5) %>% nrow()
+  SNPsSum$p1eneg5[2 + 2 * (i - 1)] <- SSRV %>% filter(P <= 1e-5) %>% nrow()
+  SNPsSum$p5eneg8[1 + 2 * (i - 1)] <- CSRV %>% filter(P <= 5e-8) %>% nrow()
+  SNPsSum$p5eneg8[2 + 2 * (i - 1)] <- SSRV %>% filter(P <= 5e-8) %>% nrow()
+  SNPsSum$Smallestp[1 + 2 * (i - 1)] <- scientific(min(CSRV$P, na.rm = TRUE), digits = 6)
+  SNPsSum$Smallestp[2 + 2 * (i - 1)] <- scientific(min(SSRV$P, na.rm = TRUE), digits = 6)
+
+  if (SNPsSum$p5eneg8[1 + 2 * (i - 1)] != 0) {
+    x <- CSRV %>% filter(P <= 5e-8) %>% mutate(Pheno = phenos[i], Exposure = "CSRV")
+    SigSNPs <- rbind(SigSNPs, x)
+  }
+  
+  if (SNPsSum$p5eneg8[2 + 2 * (i - 1)] != 0) {
+    x <- SSRV %>% filter(P <= 5e-8) %>% mutate(Pheno = phenos[i], Exposure = "SSRV")
+    SigSNPs <- rbind(SigSNPs, x)
+  }
 }
 
-SigSNPs
+SNPsSum
 
-write.table(SigSNPs, file = paste("/scratch/ahc87874/Fall2022/SNPs/SNPsTable.txt", sep = ""),
-            sep = "\t", row.names = FALSE, quote = FALSE)
+#write.table(SNPsSum, file = paste("/scratch/ahc87874/Fall2022/SNPs/SNPsTable.txt", sep = ""),
+#            sep = "\t", row.names = FALSE, quote = FALSE)
