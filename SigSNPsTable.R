147f50d278a611dffff24ff55b5f79b6c566e748
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-11-29 23:24:56 -0500

    Update SigSNPsTable.R

--- SigSNPsTable.R
+++ SigSNPsTable.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(scales)
 
 setwd("/scratch/ahc87874/Fall2022/SNPs")
 
@@ -25,8 +26,8 @@ for (i in 1:length(phenos)) {
   SigSNPs$p1eneg5[2 + 2 * (i - 1)] <- SSRV %>% filter(P <= 1e-5) %>% nrow()
   SigSNPs$p5eneg8[1 + 2 * (i - 1)] <- CSRV %>% filter(P <= 5e-8) %>% nrow()
   SigSNPs$p5eneg8[2 + 2 * (i - 1)] <- SSRV %>% filter(P <= 5e-8) %>% nrow()
-  SigSNPs$Smallestp[1 + 2 * (i - 1)] <- min(CSRV$P, na.rm = TRUE)
-  SigSNPs$Smallestp[2 + 2 * (i - 1)] <- min(SSRV$P, na.rm = TRUE)
+  SigSNPs$Smallestp[1 + 2 * (i - 1)] <- scientific(min(CSRV$P, na.rm = TRUE), digits = 5)
+  SigSNPs$Smallestp[2 + 2 * (i - 1)] <- scientific(min(SSRV$P, na.rm = TRUE), digits = 5)
 }
 
 SigSNPs
