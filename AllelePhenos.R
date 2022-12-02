1e17d60a20d8b4a9cec82fe0459f4951f36ddf5b
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-12-01 21:36:03 -0500

    Update AllelePhenos.R

--- AllelePhenos.R
+++ AllelePhenos.R
@@ -5,6 +5,8 @@ setwd("/scratch/ahc87874/Fall2022/")
 
 suffix <- "wKeep"
 
+#---------------------------------------------------------------------------------------------------------------------------------------
+#Load pheno
 pheno <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/pheno/GEMpheno", suffix, ".txt", sep = ""), sep = "\t", 
                                 header = TRUE, stringsAsFactors = FALSE))
 pheno <- pheno %>% select(-Sex, -Age, -Townsend, -starts_with("PC"))
@@ -16,6 +18,9 @@ pheno$CSRV <- replace(pheno$CSRV, pheno$CSRV == "1", "Veg")
 pheno$SSRV <- replace(pheno$SSRV, pheno$SSRV == "0", "NonVeg")
 pheno$SSRV <- replace(pheno$SSRV, pheno$SSRV == "1", "Veg")
 
-bgen.load("/scratch/ahc87874/Fall2022/geno/chr13.bgen", rsids = "rs67393898")
-"/scratch/ahc87874/Fall2022/geno/chr11.bgen", rsids = c("rs72880701", "rs1817457")
-"/scratch/ahc87874/Fall2022/geno/chr9.bgen", rsids = "9:140508031_A_G"
+#---------------------------------------------------------------------------------------------------------------------------------------
+#Load geno
+alleledir <- "/scratch/ahc87874/Fall2022/alleles/"
+
+chr13 <- as_tibble(read_delim(paste(alleledir, "chr13.raw", sep = ""), header = TRUE)
+11 9 3
