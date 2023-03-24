706b9416263299234f27edf20f07805e16140723
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-03-24 00:41:03 -0400

    Update CMPlot.R

--- CMPlot.R
+++ CMPlot.R
@@ -33,7 +33,7 @@ if (FALSE) {
   write.table(SNPs, file = "/scratch/ahc87874/Fall2022/manplots/PvalsforCMPlot.txt",
               sep = "\t", row.names = FALSE, quote = FALSE)
 } else {
-  SNPs <- as_tibble(read.table("/scratch/ahc87874/Fall2022/manplots.txt", 
+  SNPs <- as_tibble(read.table("/scratch/ahc87874/Fall2022/manplots/PvalsforCMPlot.txt", 
                                header = TRUE, stringsAsFactors = FALSE))
 }
 
@@ -45,6 +45,7 @@ toHighlight[[2]]<-SNPs$RSID[SNPs$LA_Percent_P <= 5e-8]
 toHighlight[[3]]<-SNPs$RSID[SNPs$w6_w3_Ratio_P <= 5e-08]
 toHighlight 
 
+#Suggested and Significant
 options(bitmapType='cairo')
 png(filename = "CMPlot.png", type = "cairo", width = 700, height = 700, res = 100)
 CMplot(SNPs, #dataset
@@ -69,14 +70,76 @@ CMplot(SNPs, #dataset
        chr.labels = paste("Chr", 1:22, sep = ""), #labels for chromosomes
        outward = TRUE, #plot from inside out
        file = "jpg", #file type
-       memo = "",
+       memo = "SugSig",
        dpi = 300, #resolution
        file.output = TRUE, #save as file
        width = 10,
        height = 13)
 dev.off()
 
-CMplot(pig60K,type="p",plot.type="c",r=0.4,col=c("grey30","grey60"),chr.labels=paste("Chr",c(1:18,"X","Y"),sep=""),
-      threshold=c(1e-6,1e-4),cir.chr.h=1.5,amplify=TRUE,threshold.lty=c(1,2),threshold.col=c("red",
-      "blue"),signal.line=1,signal.col=c("red","green"),chr.den.col=c("darkgreen","yellow","red"),
-      bin.size=1e6,outward=FALSE,file="jpg",memo="",dpi=300,file.output=TRUE,verbose=TRUE,width=10,height=10)
+#Only Significant
+options(bitmapType='cairo')
+png(filename = "CMPlot.png", type = "cairo", width = 700, height = 700, res = 100)
+CMplot(SNPs, #dataset
+       plot.type = "c", #circular
+       r = 1.5, #radius of circle
+       col = c("grey30", "grey60"), #regular SNP colors, alternating
+       cex = c(0.5, 0.5),
+       cir.chr.h = 1, #width of chromosome boundary
+       cir.legend.cex = 0.7, #legend text size
+       cir.legend.col = "black",
+       LOG10 = TRUE, #change P vals into log10
+       threshold = c(5e-8), #significant thresholds
+       threshold.col = c("darkred"), #threshold line colors
+       threshold.lty = c(2), #threshold line types
+       amplify = TRUE, #amplify significant SNPs
+       #highlight = toHighlight,
+       #highlight.text = unlist(toHighlight), 
+       signal.line = NULL, 
+       signal.cex = c(0.9), #significant SNP size
+       signal.pch = c(20), #significant SNP shape
+       signal.col = c("red"), #significant SNP colors
+       chr.labels = paste("Chr", 1:22, sep = ""), #labels for chromosomes
+       outward = TRUE, #plot from inside out
+       file = "jpg", #file type
+       memo = "Sig",
+       dpi = 300, #resolution
+       file.output = TRUE, #save as file
+       width = 10,
+       height = 13)
+dev.off()
+
+#Only Significant w/ colors
+options(bitmapType='cairo')
+png(filename = "CMPlot.png", type = "cairo", width = 700, height = 700, res = 100)
+CMplot(SNPs, #dataset
+       plot.type = "c", #circular
+       r = 1.5, #radius of circle
+       col = matrix(c("aquamarine3", "aquamarine1", #w3 %
+                    "gold3","gold1", #LA
+                    "mediumpurple3", "mediumpurple1", #w6 w3 ratio
+                    nrow = 3, byrow = TRUE),
+       cex = c(0.5, 0.5),
+       cir.chr.h = 1, #width of chromosome boundary
+       cir.legend.cex = 0.7, #legend text size
+       cir.legend.col = "black",
+       LOG10 = TRUE, #change P vals into log10
+       threshold = c(5e-8), #significant thresholds
+       threshold.col = c("darkred"), #threshold line colors
+       threshold.lty = c(2), #threshold line types
+       amplify = TRUE, #amplify significant SNPs
+       #highlight = toHighlight,
+       #highlight.text = unlist(toHighlight), 
+       signal.line = NULL, 
+       signal.cex = c(0.9), #significant SNP size
+       signal.pch = c(20), #significant SNP shape
+       signal.col = c("red"), #significant SNP colors
+       chr.labels = paste("Chr", 1:22, sep = ""), #labels for chromosomes
+       outward = TRUE, #plot from inside out
+       file = "jpg", #file type
+       memo = "Colored",
+       dpi = 300, #resolution
+       file.output = TRUE, #save as file
+       width = 10,
+       height = 13)
+dev.off()
