9fdca8473b64e3f90d167c1e1447243cdd97e917
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-03-28 20:39:25 -0400

    Update GetAlleles.sh

--- GetAlleles.sh
+++ GetAlleles.sh
@@ -15,15 +15,16 @@ ml PLINK/2.00-alpha2.3-x86_64-20200914-dev
 genoindir=("/scratch/ahc87874/Fall2022/geno")
 outdir=("/scratch/ahc87874/Fall2022/alleles")
 mkdir -p $outdir
-i=("9")
+i=("11")
 #13:rs67393898 11:rs72880701 11:rs1817457 11:rs149996902 9:140508031_A_G/9:rs34249205 3:rs62255849 
+#11:rs174583 FADS2
 
 plink2 \
 --bgen $genoindir/chr"$i".bgen ref-first \
 --sample $genoindir/chr"$i".sample \
---snp 9:140508031_A_G \
+--snp 11:rs174583 \
 --export A \
---out "$outdir"/chr"$i"SNP
+--out "$outdir"/chr"$i"SNP_FADS2
 
 #--snps rs72880701, rs1817457, rs149996902 \
 
