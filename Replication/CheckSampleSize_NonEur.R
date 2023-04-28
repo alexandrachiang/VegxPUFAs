276eb725e44ddf11e3d76cbe345539c832b876da
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-04-28 12:51:22 -0400

    Update CheckSampleSize_NonEur.R

--- Replication/CheckSampleSize_NonEur.R
+++ Replication/CheckSampleSize_NonEur.R
@@ -10,7 +10,16 @@ phenoQCgenoQC <- phenoQCgenoQC %>% mutate(IID = ID_1, hasGenoData = TRUE) %>% se
 
 bothQC <- left_join(pheno, phenoQCgenoQC, by = "IID")
 
-bothQC2 <- bothQC %>% select(FID, IID, 
+pan <- read_tsv("/scratch/ahc87874/Fall2022/pheno/all_pops_non_eur_pruned_within_pop_pc_covs.tsv")
+pan <- as_tibble(pan)
+pan$s <- as.integer(pan$s)
+bridge <- read.table("/scratch/ahc87874/Fall2022/pheno/ukb48818bridge31063.txt")
+bridge <- as_tibble(bridge)
+colnames(bridge) <- c("IID", "panID")
+pan2 <- pan %>% select(s, pop) %>% left_join(bridge, by = c("s" = "panID"))
+bothQC <- left_join(bothQC, pan2, by = "IID")
+
+bothQC2 <- bothQC %>% select(FID, IID,
                              sex_f31_0_0, age_when_attended_assessment_centre_f21003_0_0,
                              townsend_deprivation_index_at_recruitment_f189_0_0,
                              contains("genetic_principal_components_f22009_0_"), 
@@ -18,7 +27,7 @@ bothQC2 <- bothQC %>% select(FID, IID,
                              w3FA_NMR, w3FA_NMR_TFAP, w6FA_NMR, w6FA_NMR_TFAP, w6_w3_ratio_NMR, 
                              DHA_NMR, DHA_NMR_TFAP, LA_NMR, LA_NMR_TFAP, PUFA_NMR, PUFA_NMR_TFAP, 
                              MUFA_NMR, MUFA_NMR_TFAP, PUFA_MUFA_ratio_NMR,
-                             hasPCA, hasGenoData)
+                             hasPCA, hasGenoData, pop)
 
 #Rename columns and change order
 covars <- c("Sex", "Age", "Townsend", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10")
@@ -34,29 +43,23 @@ table(bothQC4, useNA = "always")
 #       hasGenoData
 #hasPCA   FALSE   TRUE   <NA>
 #  FALSE   4063      0      0
-#  TRUE   51457 155182      0
+#  TRUE  200307   6363      0
 #  <NA>       0      0      0
 
-bothQC5 <- bothQC3 %>% filter(hasGenoData == TRUE) %>% select(CSRV, SSRV)
+bothQC5 <- bothQC3 %>% filter(hasGenoData == TRUE) %>% select(CSRV, SSRV, pop)
 
 table(bothQC$CSRV, useNA = "always")
 #NonVeg    Veg   <NA>
-#202724   8243      0
+#202750   8248      0
 table(bothQC$SSRV, useNA = "always")
 #NonVeg    Veg   <NA>
-#202724   3271   4972
+#202750   3271   4977
 table(bothQC5$CSRV, useNA = "always")
 #NonVeg    Veg   <NA>
-#149572   5610      0
+#  5564    799      0
 table(bothQC5$SSRV, useNA = "always")
 #NonVeg    Veg   <NA>
-#149572   2369   3241
-
-#Difference
-#CSRV NonVeg = 53152
-#CSRV Veg = 2633
-#SSRV NonVeg = 53152
-#SSRV Veg = 902
+#  5564    233    566
 
 colSums(is.na(bothQC2)) %>% as.data.frame()
 #FID                      0
@@ -75,35 +78,53 @@ colSums(is.na(bothQC2)) %>% as.data.frame()
 #PC9                   4072
 #PC10                  4072
 #CSRV                     0
-#SSRV                 4972
-#w3FA                161136
-#w3FA_TFAP           161136
-#w6FA                161136
-#w6FA_TFAP           161136
-#w6_w3_ratio         161136
-#DHA                 161136
-#DHA_TFAP            161136
-#LA                  161136
-#LA_TFAP             161136
-#PUFA                161136
-#PUFA_TFAP           161136
-#MUFA                161136
-#MUFA_TFAP           161136
-#PUFA_MUFA_ratio     161136
+#SSRV                  4977
+#w3FA_NMR            161158
+#w3FA_NMR_TFAP       161158
+#w6FA_NMR            161158
+#w6FA_NMR_TFAP       161158
+#w6_w3_ratio_NMR     161158
+#DHA_NMR             161158
+#DHA_NMR_TFAP        161158
+#LA_NMR              161158
+#LA_NMR_TFAP         161158
+#PUFA_NMR            161158
+#PUFA_NMR_TFAP       161158
+#MUFA_NMR            161158
+#MUFA_NMR_TFAP       161158
+#PUFA_MUFA_ratio_NMR 161158
 #hasPCA                   0
 #hasGenoData              0
+#pop                  21066
 
-cc <- c("IID", "Sex", "Age", "Townsend", "PC1", "w3FA")
+cc <- c("IID", "Sex", "Age", "Townsend", "PC1", "w3FA_NMR")
 bothQC6 <- bothQC2[complete.cases(bothQC2[, cc]), ]
 bothQC6 <- bothQC6 %>% filter(hasGenoData == TRUE) #37,106
 #colSums(is.na(bothQC6)) %>% as.data.frame() #onlt SSRV should have NA
 
 table(bothQC6$CSRV, useNA = "always")
 #NonVeg    Veg   <NA>
-# 35780   1326      0
+#  1317    194      0
 table(bothQC6$SSRV, useNA = "always")
 #NonVeg    Veg   <NA>
-# 35780    568    758
+#  1317     47    147
+table(bothQC6$pop, useNA="always")
+# AFR  AMR  CSA  EAS  MID <NA>
+# 448  106  624  227  106    0
+
+bothQC6 %>% select(CSRV, pop) %>% table(useNA="always")
+#        pop
+#CSRV     AFR AMR CSA EAS MID <NA>
+#  NonVeg 431 103 460 220 103    0
+#  Veg     17   3 164   7   3    0
+#  <NA>     0   0   0   0   0    0
+bothQC6 %>% select(SSRV, pop) %>% table(useNA="always")
+#        pop
+#SSRV     AFR AMR CSA EAS MID <NA>
+#  NonVeg 431 103 460 220 103    0
+#  Veg      1   0  45   1   0    0
+#  <NA>    16   3 119   6   3    0
+
 
 #Difference
 #CSRV NonVeg = 166944
