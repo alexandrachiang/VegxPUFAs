b013f611cbc34530a425b220002b5a596204c392
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-07-23 13:34:14 -0400

    Update qqmanFinal.R

--- Plots/qqmanFinal.R
+++ Plots/qqmanFinal.R
@@ -55,8 +55,8 @@ for (i in phenos) {
 
       if (i == "w3FA_NMR_TFAP") {
         phe <- "w3 %"
-	infileall$SNP[infileall$SNP == "9:140508031_A_G"] <- "rs34249205"
-	SNPs <- "rs34249205"
+      	infileall$SNP[infileall$SNP == "9:140508031_A_G"] <- "rs34249205"
+      	SNPs <- "rs34249205"
       } else if (i == "w6_w3_ratio_NMR") {
         phe <- "w6/w3 Ratio"
         SNPs <- "rs72880701"
@@ -64,43 +64,51 @@ for (i in phenos) {
         phe <- "LA %"
         SNPs <- c("rs1817457", "rs149996902")
       }
+
+      #Colors
+      if (i == "w3FA_NMR_TFAP") {
+        colors = c()
+      } else if (i == "") {
+        colors = c()
+      } else if (i == "") {
+        colors = c()
+      }
 	   
       print("Manhattan")
       #Make manhattan plot
       outdirman = "/scratch/ahc87874/Fall2022/manplots/"
       png(filename = paste(outdirman, "FINAL", i, "man.png", sep = ""), type = "cairo", width = 1500, height = 750, res = 110)
-      manhattancex(infileall, suggestiveline = -log10(5e-05), genomewideline = -log10(5e-08), col = c("#141a17", "#7b998a"),
+      manhattancex(infileall, suggestiveline = -log10(5e-05), genomewideline = -log10(5e-08), col = colors,
                    main = paste("Manhattan Plot of", phe, "GWIS", sep = " "), annotatePval = 5e-5, ylim = c(0, -log10(1e-08)), 
                    annofontsize = 1, cex.axis = 1.3, cex.lab = 1.3, cex.main = 1.7, highlight = SNPs, 
 		   highlightcol = "#ff0000", highlighttextcol = "#ff0000")
       dev.off()
 
-      #print("QQ")
-      #Make qq plot
-      #outdirqq = "/scratch/ahc87874/Fall2022/qqplots/"
+      print("QQ")
+      Make qq plot
+      outdirqq = "/scratch/ahc87874/Fall2022/qqplots/"
 	    
-      #png(filename = paste(outdirqq, "FINAL", i, "qq.png", sep = ""), type = "cairo", width = 600, height = 600)
-      #qq(infileall$P, main = paste("Q-Q Plot of", phe, "P-Values", sep = " "))
-      #dev.off()
+      png(filename = paste(outdirqq, "FINAL", i, "qq.png", sep = ""), type = "cairo", width = 600, height = 600)
+      qq(infileall$P, main = paste("Q-Q Plot of", phe, "P-Values", sep = " "))
+      dev.off()
       
-      if (i == "w3FA_NMR_TFAP") {
-        print("MAGMA")
-        #Make MAGMA plot
-        magma <-  as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/magma.genes.", i, ".txt", sep = ""), 
-                                            header = TRUE, stringsAsFactors = FALSE))
+      
+      print("MAGMA")
+      #Make MAGMA plot
+      magma <-  as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/FUMA/magma.genes.", i, ".txt", sep = ""), 
+                                          header = TRUE, stringsAsFactors = FALSE))
 
-        magma <- magma %>% mutate(MIDDLE = (START + STOP)/2) %>% select(CHR, MIDDLE, P, SYMBOL)
-        colnames(magma) <- c("CHR", "BP", "P", "SNP")
-        SNPs <- c("C9orf37", "ARRDC1")
+      magma <- magma %>% mutate(MIDDLE = (START + STOP)/2) %>% select(CHR, MIDDLE, P, SYMBOL)
+      colnames(magma) <- c("CHR", "BP", "P", "SNP")
+      SNPs <- c("C9orf37", "ARRDC1")
 
-        outdirmagma = "/scratch/ahc87874/Fall2022/MAGMAplots/"
-        png(filename = paste(outdirmagma, i, "MAGMA.png", sep = ""), type = "cairo", width = 1000, height = 400, res = 105)
-        manhattancex(magma, suggestiveline = FALSE, genomewideline = -log10(2.619e-6), #col = c("#15141a", "#807b99"),
-                     main = paste("Manhattan Plot of", phe, "Gene-Based Test by MAGMA", sep = " "), 
-                     annotatePval = -log10(2.619e-6), ylim = c(0, -log10(1e-08)), annofontsize = 1, cex.axis = 1.3, 
-		     cex.lab = 1.3, cex.main = 1.7, highlight = SNPs, highlightcol = "#ff0000", highlighttextcol = "#ff0000")
-        dev.off()
-      }  
+      outdirmagma = "/scratch/ahc87874/Fall2022/MAGMAplots/"
+      png(filename = paste(outdirmagma, i, "MAGMA.png", sep = ""), type = "cairo", width = 1000, height = 400, res = 105)
+      manhattancex(magma, suggestiveline = FALSE, genomewideline = -log10(2.619e-6), col = colors,
+                   main = paste("Manhattan Plot of", phe, "Gene-Based Test by MAGMA", sep = " "), 
+                   annotatePval = -log10(2.619e-6), ylim = c(0, -log10(1e-08)), annofontsize = 1, cex.axis = 1.3, 
+                   cex.lab = 1.3, cex.main = 1.7, highlight = SNPs, highlightcol = "#ff0000", highlighttextcol = "#ff0000")
+      dev.off() 
     } #j exposures
   } #i phenos
 
