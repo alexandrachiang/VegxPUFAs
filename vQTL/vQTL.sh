579f31c6f74a7f90a1302c39ce9cc1814f7f493f
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-09-18 12:02:26 -0400

    Update vQTL.sh

--- vQTL/vQTL.sh
+++ vQTL/vQTL.sh
@@ -11,19 +11,26 @@
 #SBATCH --mail-type=ALL
 #SBATCH --array=1-2
 
-i=$SLURM_ARRAY_TASK_ID
-
-genodir=("/scratch/ahc87874/Fall2022/geno")
-phenodir=("/scratch/ahc87874/Fall2022/vQTL")
-
 cd /scratch/ahc87874/Fall2022/vQTL
+
+i=$SLURM_ARRAY_TASK_ID
+phenotypes=("w3FA_NMR" "w3FA_NMR_TFAP" "w6FA_NMR" "w6FA_NMR_TFAP" "w6_w3_ratio_NMR" 
+"DHA_NMR" "DHA_NMR_TFAP" "LA_NMR" "LA_NMR_TFAP" "PUFA_NMR" "PUFA_NMR_TFAP" "MUFA_NMR" 
+"MUFA_NMR_TFAP" "PUFA_MUFA_ratio_NMR")
+genodir=("/scratch/ahc87874/Fall2022/geno")
+phenodir=("/scratch/ahc87874/Fall2022/pheno/INT")
+
+for j in ${phenotypes[@]}
+  do
 
 ./osca-0.46.1 \
 --vqtl \
 --bfile $genodir/chr"$i".bgen \
---pheno $phenodir/INTpheno.csv \
+--pheno $phenodir/"$j"INT.txt \
 --vqtl-mtd 0 \
 --task-num 1000 \
 --task-id 1 \
 --thread-num 10 \
 --out vQTL_Bartlett_chr"$i"
+
+done
