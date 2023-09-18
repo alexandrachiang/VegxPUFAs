c9c2d38e24be833ca08b33314c8fdc82a3f9ba25
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-09-18 11:56:16 -0400

    Update INT.R

--- vQTL/INT.R
+++ vQTL/INT.R
@@ -5,13 +5,19 @@ library(tidyverse)
 setwd("/scratch/ahc87874/Fall2022/pheno")
 
 pheno <- as_tibble(read.csv("/scratch/ahc87874/Fall2022/pheno/GEMphenowKeep.csv", header = TRUE, stringsAsFactors = FALSE))
-pheno2 <- pheno
+pheno2 <- pheno[, 19:31]
 
-for (i in 19:31) {
-  pheno2[, i] <- qnorm((rank(pheno[, i],na.last="keep")-0.5)/sum(!is.na(pheno[, i])))
+for (i in 1:13) {
+  pheno2[, i] <- qnorm((rank(pheno2[, i],na.last="keep")-0.5)/sum(!is.na(pheno2[, i])))
 }
 
 write.csv(pheno2, file = "INTpheno.csv", row.names = FALSE, quote = FALSE)
+
+for (i in 1:13) {
+  phenoname <- name(pheno2)[i]
+  phenolist <- as.vector(pheno2[, i])
+  write.table(phenolist, paste(phenoname, "INT.txt", sep = ""), row.names = FALSE, quote = FALSE)
+}
 
 #png(filename = "pheno.png", type = "cairo", width = 700, height = 700)
 #print(hist(pheno$w3FA_NMR_TFAP))
