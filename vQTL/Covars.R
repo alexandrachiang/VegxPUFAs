2a2f632ac9ff4b8b8f7e20062dc4e9c1973b6647
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-09-22 15:41:15 -0400

    Update Covars.R

--- vQTL/Covars.R
+++ vQTL/Covars.R
@@ -4,3 +4,10 @@ setwd("/scratch/ahc87874/Fall2022/pheno/INT")
 
 pheno <- as_tibble(read.csv("/scratch/ahc87874/Fall2022/pheno/GEMphenowKeep.csv", header = TRUE, stringsAsFactors = FALSE))
 
+#covars - discrete - sex and age
+covars <- pheno[, c(1:4)]
+write.table(covars, "covarsINT.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+#qcovars - quantitative - townsend and pca
+qcovars <- pheno[, c(1, 2, 5:15)]
+write.table(qcovars, "qcovarsINT.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
