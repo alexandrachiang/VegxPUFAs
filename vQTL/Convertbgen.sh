a43b70c60d6c14c5475d4eae7d2b0188a6093f27
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-09-22 20:30:00 -0400

    Update Convertbgen.sh

--- vQTL/Convertbgen.sh
+++ vQTL/Convertbgen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=batch
+#SBATCH --partition=highmem_p
 #SBATCH --job-name=Convertbgen
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --time=70:00:00
-#SBATCH --mem=30000
+#SBATCH --mem=300000
 #SBATCH --output=Convertbgen.%j.out
 #SBATCH --error=Convertbgen.%j.err
 #SBATCH --mail-user=ahc87874@uga.edu
