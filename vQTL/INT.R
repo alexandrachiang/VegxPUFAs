db6dd4d025be430e63a8547b231d50ef4b2d9d99
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-09-24 13:13:44 -0400

    Update INT.R

--- vQTL/INT.R
+++ vQTL/INT.R
@@ -5,7 +5,7 @@ library(tidyverse)
 setwd("/scratch/ahc87874/Fall2022/pheno/INT")
 
 pheno <- as_tibble(read.csv("/scratch/ahc87874/Fall2022/pheno/GEMphenowKeep.csv", header = TRUE, stringsAsFactors = FALSE))
-pheno2 <- pheno[, 19:31]
+pheno2 <- pheno[, 18:31]
 
 for (i in 1:13) {
   pheno2[, i] <- qnorm((rank(pheno2[, i],na.last="keep")-0.5)/sum(!is.na(pheno2[, i])))
@@ -15,7 +15,7 @@ pheno3 <- cbind (pheno$FID, pheno$IID, pheno2)
 names(pheno3)[1:2] <- c("FID", "IID")
 write.csv(pheno3, file = "INTpheno.csv", row.names = FALSE, quote = FALSE)
 
-for (i in 3:15) {
+for (i in 3:16) {
   phenoname <- names(pheno3)[i]
   phenolist <- pheno3[, c(1, 2, i)]
   write.table(phenolist, paste(phenoname, "INT.txt", sep = ""), row.names = FALSE, col.names = FALSE, quote = FALSE)
