506e0b492d3ceae09ccf3471bea1c3f090343747
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-09-24 12:56:49 -0400

    Create vQTLMan.sh

---
+++ vQTL/vQTLMan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=vQTLMan
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=20:00:00
+#SBATCH --mem=122GB
+#SBATCH --output=vQTLMan.%j.out
+#SBATCH --error=vQTLMan.%j.err
+#SBATCH --mail-user=ahc87874@uga.edu
+#SBATCH --mail-type=ALL
+
+cd $SLURM_SUBMIT_DIR
+
+module load R/4.3.1-foss-2022a
+R CMD BATCH vQTLMan.R
