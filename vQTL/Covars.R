92687611f0dfa7515439d8b0fed95696077ace74
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-02 00:53:58 -0400

    Update Covars.R

--- vQTL/Covars.R
+++ vQTL/Covars.R
@@ -4,10 +4,12 @@ setwd("/scratch/ahc87874/Fall2022/pheno/INT")
 
 pheno <- as_tibble(read.csv("/scratch/ahc87874/Fall2022/pheno/GEMphenowKeep.csv", header = TRUE, stringsAsFactors = FALSE))
 
-#covars - discrete - sex and age
-covars <- pheno[, c(1:4)]
+#covars - discrete - sex
+covars <- pheno[, c(1:3)]
 write.table(covars, "covarsINT.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-#qcovars - quantitative - pca
-qcovars <- pheno[, c(1, 2, 5:15)] # remove ts
+#qcovars - quantitative - age and pca
+qcovars <- pheno[, c(1, 2, 4, 6:15)] 
 write.table(qcovars, "qcovarsINT.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+#doesnt matter if age is in covars or qcovars
