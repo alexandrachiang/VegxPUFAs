8a87c1e30350958148c2319886ba2128d80c5ee1
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-29 12:17:51 -0400

    Update and rename writenmr.sh to FishOilCombined.sh

--- FishOil/FishOilCombined.sh
+++ FishOil/FishOilCombined.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=highmem_p
-#SBATCH --job-name=writenmr
+#SBATCH --job-name=FishOilCombined
 #SBATCH --ntasks=16
 #SBATCH --nodes=1
 #SBATCH --time=1:00:00
 #SBATCH --mem=130GB
-#SBATCH --output=writenmr.%j.out
-#SBATCH --error=writenmr.%j.err
+#SBATCH --output=FishOilCombined.%j.out
+#SBATCH --error=FishOilCombined.%j.err
 #SBATCH --mail-user=ahc87874@uga.edu
 #SBATCH --mail-type=ALL
 
 cd $SLURM_SUBMIT_DIR
 
 module load R/4.3.1-foss-2022a
-R CMD BATCH writenmr.R
+R CMD BATCH FishOilCombined.R
