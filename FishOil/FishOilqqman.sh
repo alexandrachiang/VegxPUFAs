7649ec123c39038e41366c9ab4ada9875391da0e
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-29 20:08:17 -0400

    Create FishOilqqman.sh

---
+++ FishOil/FishOilqqman.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=highmem_p
+#SBATCH --job-name=FishOilqqman
+#SBATCH --ntasks=16
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=200GB
+#SBATCH --output=FishOilqqman.%j.out
+#SBATCH --error=FishOilqqman.%j.err
+#SBATCH --mail-user=ahc87874@uga.edu
+#SBATCH --mail-type=ALL
+
+cd $SLURM_SUBMIT_DIR
+
+module load R/4.3.1-foss-2022a
+R CMD BATCH FishOilqqman.R
