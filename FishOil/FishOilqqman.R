c4be471f37fb72139260649bc5ee60b59fc6f289
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-30 12:48:15 -0400

    Update FishOilqqman.R

--- FishOil/FishOilqqman.R
+++ FishOil/FishOilqqman.R
@@ -12,13 +12,16 @@ phenos <- c("w3FA", "w3FA_TFAP", "w6FA", "w6FA_TFAP", "w6_w3_ratio", "DHA",
 
 exposures <- c("Fish_oil_baseline")
 
-for (i in phenos) {
+suffix <- c("comb", "phase1", "phase2")
+
+for (k in suffix) {
+	for (i in phenos) {
     print(paste("pheno:", i))
 
     for (j in exposures) {
       print(paste("exposure:", j))
      
-      infileall <- as_tibble(read.table(paste("/scratch/ahc87874/FishOil/Combined/", i, "x", j, "alltab.txt", sep = ""), 
+      infileall <- as_tibble(read.table(paste("/scratch/ahc87874/FishOil/Combined/", i, "x", j, k, "alltab.txt", sep = ""), 
                                           header = TRUE, stringsAsFactors = FALSE))
 	    
 	    infileall <- infileall %>% select(CHR, POS, robust_P_Value_Interaction, RSID)
@@ -28,13 +31,13 @@ for (i in phenos) {
       #Make table of sig SNPs (P < 1e-5)
       outdirSNPs = "/scratch/ahc87874/FishOil/SNPs/"
       sigSNPs <- infileall %>% filter(P <= 1e-5)
-      write.table(sigSNPs, paste(outdirSNPs, i, "x", j, "sigSNPs.txt", sep = ""),
+      write.table(sigSNPs, paste(outdirSNPs, i, "x", j, k, "sigSNPs.txt", sep = ""),
                   row.names = FALSE, quote = FALSE)
   
       #Make table of top 10 SNPs
       newdata <- infileall[order(infileall$P), ]
       newdata <- newdata[1:10, ]
-      write.table(newdata, paste(outdirSNPs, i, "x", j, "topSNPs.txt", sep = ""),
+      write.table(newdata, paste(outdirSNPs, i, "x", j, k, "topSNPs.txt", sep = ""),
                   row.names = FALSE, quote = FALSE)
   
       pvalue <- newdata$P[10]
@@ -49,7 +52,7 @@ for (i in phenos) {
         maxy <- -log10(newdata$P[1])
       }
 	    
-      png(filename = paste(outdirman, i, "x", j, "man.png", sep = ""), type = "cairo", width = 1200, height = 600)
+      png(filename = paste(outdirman, i, "x", j, k, "man.png", sep = ""), type = "cairo", width = 1200, height = 600)
       manhattancex(infileall, suggestiveline = -log10(1e-05), genomewideline = -log10(5e-08),
                    main = paste("Manhattan Plot of ", i, " x ", expo, " GWIS", sep = ""), annotatePval = 1e-5, ylim = c(0, maxy + 0.15), 
                    annofontsize = 1, cex.axis = 1.3, cex.lab = 1.3, cex.main = 1.7)
@@ -59,10 +62,11 @@ for (i in phenos) {
       #Make qq plot
       outdirqq = "/scratch/ahc87874/FishOil/qqplots/"
 	    
-      png(filename = paste(outdirqq, i, "x", j, "qq.png", sep = ""), type = "cairo", width = 600, height = 600)
+      png(filename = paste(outdirqq, i, "x", j, k, "qq.png", sep = ""), type = "cairo", width = 600, height = 600)
       qq(infileall$P, main = paste("Q-Q plot of ", i, " x ", expo, " GWIS p-values", sep = ""))
       dev.off()
      
     } #j exposures
   } #i phenos
+} #k suffix
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
