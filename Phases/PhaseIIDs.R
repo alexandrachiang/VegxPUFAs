e9ab41b533992b7c7e3da9743d0272fbf2c599b0
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-29 20:50:50 -0400

    Update PhaseIIDs.R

--- Phases/PhaseIIDs.R
+++ Phases/PhaseIIDs.R
@@ -7,13 +7,22 @@ suppressMessages(library(ukbtools)) #<3
 suppressMessages(library(rio))
 
 #Load dataset
-setwd("/scratch/ahc87874/Fall2022/pheno/")
+setwd("/scratch/ahc87874/Phase/pheno/")
 
+withdrawn <-read.csv("/scratch/ahc87874/Fall2022/pheno/w48818_2023-04-25.csv", header = FALSE)
 
+phase1 <- as_tibble(read.table("/scratch/ahc87874/Fall2022/pheno/PUFAs.txt", header = TRUE))
+phase1 <- phase1[!(phase1$IID %in% withdrawn$V1), ] 
+phase1 <- phase1 %>% filter(!is.na(w3FA_NMR)) %>% select(IID)
 
 setwd("/scratch/ahc87874/Fall2022/pheno/673621/")
 phasecomb <- ukb_df("ukb673621", n_threads = "max", data.pos = 2)
 phasecomb <- as_tibble(phasecomb)
-phasecomb <- phasecomb %>% select(eid)
-                                                         
-write.csv(phasecomb, file = "/scratch/ahc87874/Phase/pheno/phasecombIIDs.csv", row.names = FALSE, quote = FALSE)
+phasecomb %>% select(eid, contains("f23444_0_0")) %>% head()
+
+#phasecomb <- phasecomb %>% filter(!is.na(w3FA)) %>% select(eid)
+#phasecomb <- phasecomb[!(phasecomb$eid %in% withdrawn$V1), ]
+#names(phasecomb) <- "IID"
+#phase2 <- subset(phasecomb, !(IID %in% phase1$IID))
+
+#write.csv(phasecomb, file = "/scratch/ahc87874/Phase/pheno/phasecombIIDs.csv", row.names = FALSE, quote = FALSE)
