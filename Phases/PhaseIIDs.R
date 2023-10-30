c30a6033fb3e535f90e68461ca65c30d4630af8f
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-29 20:29:49 -0400

    Update PhaseIIDs.R

--- Phases/PhaseIIDs.R
+++ Phases/PhaseIIDs.R
@@ -1,1 +1,19 @@
+#Load packages and set up wd
+suppressMessages(library(plyr))
+suppressMessages(library(dplyr))
+suppressMessages(library(data.table))
+suppressMessages(library(tidyverse))
+suppressMessages(library(ukbtools)) #<3
+suppressMessages(library(rio))
 
+#Load dataset
+setwd("/scratch/ahc87874/Fall2022/pheno/")
+
+
+
+setwd("/scratch/ahc87874/Fall2022/pheno/673621/")
+phasecomb <- ukb_df("ukb673621", n_threads = "max", data.pos = 2)
+phasecomb <- as_tibble(phasecomb)
+phasecomb <- phasecomb %>% select(eid)
+                                                         
+write.csv(phasecomb, file = "/scratch/ahc87874/Phase/pheno/phasecombIIDs.csv", row.names = FALSE, quote = FALSE)
