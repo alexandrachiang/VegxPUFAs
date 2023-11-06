17a24bd46692133692b26961029dc44d44e1a58f
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-11-06 12:01:49 -0500

    Update MakeGEMPhenoFishOil.R

--- FishOil/MakeGEMPhenoFishOil.R
+++ FishOil/MakeGEMPhenoFishOil.R
@@ -5,11 +5,18 @@ library(fastDummies)
 
 fishoil <- as_tibble(read.table("/scratch/ahc87874/FishOil/pheno/phenosfish.txt", sep = "\t", 
                                 header = TRUE, stringsAsFactors = FALSE))
+phenoQCgenoQC <- as_tibble(read.table("/scratch/ahc87874/Fall2022/geno/chr22.sample", 
+                               header = TRUE, stringsAsFactors = FALSE))
 
 phase1 <- as_tibble(import("/scratch/ahc87874/Phase/pheno/phase1IIDs.csv"))
 phase2 <- as_tibble(import("/scratch/ahc87874/Phase/pheno/phase2IIDs.csv"))
 phasecomb <- as_tibble(import("/scratch/ahc87874/Phase/pheno/phasecombIIDs.csv"))
 
+#Remove withdrawn participants from dataset
+withdrawn <- read.csv("/scratch/ahc87874/Fall2022/pheno/w48818_2023-04-25.csv", header = FALSE)
+fishoil <- fishoil[!(fishoil$IID %in% withdrawn$V1), ]
+
+# Select columns
 GEMpheno <- fishoil %>% select(FID, IID, 
                                sex_f31_0_0, age_when_attended_assessment_centre_f21003_0_0, agesex,                                 ,
                                contains("genetic_principal_components_f22009_0_"), 
@@ -28,14 +35,22 @@ GEMpheno2$Sex <- replace(GEMpheno2$Sex, GEMpheno2$Sex == "Female", 0)
 GEMpheno2$Sex <- replace(GEMpheno2$Sex, GEMpheno2$Sex == "Male", 1)
 GEMpheno2$Sex <- as.numeric(GEMpheno2$Sex)
 
-# Remove non complete cases
-GEMpheno3 <- GEMpheno2
+# Remove if missing genetic data or doesnt pass geno/pheno QC
+phenoQCgenoQC <- phenoQCgenoQC %>% mutate(IID = ID_1, hasGenoData = TRUE) %>% select(IID, hasGenoData)
+GEMpheno3 <- subset(GEMpheno2, (IID %in% phenoQCgenoQC$IID))
+
+# Remove if missing covariate data
+GEMpheno3 <- GEMpheno3 %>% filter(!is.na(Sex), !is.na(Age), !is.na(AgeSex), !is.na(PC1))
+
+# Remove if missing phenotype data
+GEMpheno3 <- GEMpheno3 %>% filter(!is.na(Fish_oil_baseline), !is.na(w3FA))
 
 # Subset by Phase IID
 PUFAsINTcomb <- subset(GEMpheno3, (IID %in% phasecomb$IID))
 PUFAsINT1 <- subset(GEMpheno3, (IID %in% phase1$IID))
 PUFAsINT2 <- subset(GEMpheno3, (IID %in% phase2$IID))
 
+# INT
 for (i in 2:15) {
   print(names(PUFAsINTcomb)[i])
   PUFAsINTcomb[, i] <- qnorm((rank(PUFAsINTcomb[, i],na.last="keep")-0.5)/sum(!is.na(PUFAsINTcomb[, i])))
