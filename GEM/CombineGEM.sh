486844c13a9f88edbd36946f739af0cd20fe1181
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-11-22 03:06:37 -0500

    Update CombineGEM.sh

--- GEM/CombineGEM.sh
+++ GEM/CombineGEM.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=batch
 #SBATCH --job-name=CombineGEM
+#SBATCH --ntasks=16
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
-#SBATCH --time=20:00:00
-#SBATCH --mem=40GB
+#SBATCH --time=6:00:00
+#SBATCH --mem=90GB
 #SBATCH --output=CombineGEM.%j.out
 #SBATCH --error=CombineGEM.%j.err
 #SBATCH --mail-user=ahc87874@uga.edu
@@ -12,5 +12,5 @@
 
 cd $SLURM_SUBMIT_DIR
 
-module load R/4.1.3-foss-2020b
+module load R/4.3.1-foss-2022a
 R CMD BATCH CombineGEM.R
