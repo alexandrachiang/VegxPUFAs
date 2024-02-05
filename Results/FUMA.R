5856899712856a491cb8e789d5eac78ff7de1c98
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-02-04 23:09:36 -0500

    Update FUMA.R

--- Results/FUMA.R
+++ Results/FUMA.R
@@ -7,8 +7,8 @@ GEMfiles <- list.files("/scratch/ahc87874/Fall2022/Combined", pattern="*comb.txt
 
 for (i in 1:length(GEMfiles)) {
   temp <- import(GEMfiles[i])
-  temp <- temp %>% select(RSID, CHR, POS, Non_Effect_Allele, Effect_Allele, N_Samples, Beta_G.SSRV, 
-                          robust_SE_Beta_G.SSRV, robust_P_Value_Interaction)
+  temp <- temp %>% select(RSID, CHR, POS, Non_Effect_Allele, Effect_Allele, N_Samples, Beta_G.Vegetarian, 
+                          robust_SE_Beta_G.Vegetarian, robust_P_Value_Interaction)
   write.table(temp, paste(GEMfiles[i], "reducedcomb.txt", sep = ""), 
                     row.names = FALSE, quote = FALSE, sep = "\t")
 }
