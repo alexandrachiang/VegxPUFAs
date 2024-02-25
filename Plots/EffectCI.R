1046202c35ea7d20e105a8d08100de8c548b4f9f
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-02-25 11:11:13 -0500

    Update EffectCI.R

--- Plots/EffectCI.R
+++ Plots/EffectCI.R
@@ -11,23 +11,39 @@ for (i in suffix) {
   print(paste("DHA", i))
   DHAData <- as_tibble(read.table(paste("DHA", "xVegetarianVeg", i, ".txt", sep = ""), 
                                          header = TRUE, stringsAsFactors = FALSE))
-  DHAData %>% filter(CHR == 8, POS == 52231394) %>% print(width=Inf)
+  temp <- DHAData %>% filter(CHR == 8, POS == 52231394) %>% mutate(Pheno = "DHA", Phase = i) %>% select(Pheno, Phase, everything()) 
+  temp %>% print(width=Inf)
+  if (i == "comb") {
+    allData <- temp
+  } else {
+    allData <- rbind(allData, temp)
+  }
 
   print("")
   print(paste("PUFA", i))
   PUFAData <- as_tibble(read.table(paste("PUFA", "xVegetarianVeg", i, ".txt", sep = ""), 
                                          header = TRUE, stringsAsFactors = FALSE))
-  PUFAData %>% filter(CHR == 2, POS == 85067224) %>% print(width=Inf)
-
+  temp <- PUFAData %>% filter(CHR == 2, POS == 85067224) %>% mutate(Pheno = "PUFA", Phase = i) %>% select(Pheno, Phase, everything()) 
+  temp %>% print(width=Inf)
+  allData <- rbind(allData, temp)
+    
   print("")
   print(paste("w3FA_TFAP", i))
   w3FA_TFAPData <- as_tibble(read.table(paste("w3FA_TFAP", "xVegetarianVeg", i, ".txt", sep = ""), 
                                          header = TRUE, stringsAsFactors = FALSE))
-  w3FA_TFAPData %>% filter(CHR == 8, POS == 52231394) %>% print(width=Inf)
-
+  temp <- w3FA_TFAPData %>% filter(CHR == 8, POS == 52231394) %>% mutate(Pheno = "w3FA_TFAP", Phase = i) %>% select(Pheno, Phase, everything()) 
+  temp %>% print(width=Inf)
+  allData <- rbind(allData, temp)
+  
   print("")
   print(paste("w3FA", i))
   w3FAData <- as_tibble(read.table(paste("w3FA", "xVegetarianVeg", i, ".txt", sep = ""), 
                                          header = TRUE, stringsAsFactors = FALSE))
-  w3FAData %>% filter(CHR == 8, POS == 52486885) %>% print(width=Inf)
+  temp <- w3FAData %>% filter(CHR == 8, POS == 52486885) %>% mutate(Pheno = "w3FA_TFAP", Phase = i) %>% select(Pheno, Phase, everything()) 
+  temp %>% print(width=Inf)
+  allData <- rbind(allData, temp)
 }
+
+outdir <- "/scratch/ahc87874/Fall2022/Combined"
+write.table(allData, paste(outdir, "MAGMASNPs.txt", sep = "/"), 
+            row.names = FALSE, quote = FALSE, sep = "\t")
