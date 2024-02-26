0a69c0def41f1905319212abab69741701578d0a
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-02-25 19:45:56 -0500

    Update convertpgen.sh

--- GenoQC/convertpgen.sh
+++ GenoQC/convertpgen.sh
@@ -12,7 +12,6 @@
 
 #Load PLINK
 ml PLINK/2.00a4-GCC-11.2.0
-
 chr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22)
 
 #Set genotype directory
@@ -24,12 +23,9 @@ mkdir -p $outdir
 
 for i in ${chr[@]}
 do
-
-#RUN PLINK2
 plink2 \
 --bgen $genodir/ukb_imp_chr"$i"_v3.bgen ref-first \
 --sample $genodir/ukb_imp_v3.sample \
 --make-pgen \
 --out $outdir/chr"$i"
-
 done
