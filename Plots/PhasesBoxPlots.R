e75a2aab8eced0c1fbb334f491a64928e3b53fd3
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-02-25 21:45:30 -0500

    Update PhasesBoxPlots.R

--- Plots/PhasesBoxPlots.R
+++ Plots/PhasesBoxPlots.R
@@ -57,32 +57,23 @@ if (FALSE) {
   #6       13  98799253 rs67393898      G     T
 
   #RSID	      CHR	POS	    Non_Effect_Allele	Effect_Allele
-  #rs4873543	8	52231394	A	G
   #rs80103778	2	85067224	C	G
   #rs4873543	8	52231394	A	G
   #rs6985833	8	52486885	G	T
 
   #majorallele_minorallele
-  names(alleles)[(ncol(alleles) - 5):ncol(alleles)] <- c("rs62255849_T_C", "rs34249205_A_G", "rs72880701_G_T", 
-                                                         "rs1817457_G_A", "rs149996902_CT_C", "rs67393898_G_T")
+  names(alleles)[(ncol(alleles) - 2):ncol(alleles)] <- c("rs80103778_G_C", "rs4873543_G_A", "rs6985833_T_G")
   
   #---------------------------------------------------------------------------------------------------------------------------------------
   #Complete cases
   #colSums(is.na(df))
-  alleles <- alleles[complete.cases(alleles$w3FA), ]
-  alleles <- alleles[complete.cases(alleles$rs62255849_T_C), ]
+  #alleles <- alleles[complete.cases(alleles$w3FA), ]
+  #alleles <- alleles[complete.cases(alleles$rs62255849_T_C), ]
   
-  #alleles %>% select(CSRV,SSRV) %>% table(useNA="always")
-  #          SSRV
-  #CSRV     NonVeg   Veg  <NA>
-  #  NonVeg  35780     0     0
-  #  Veg         0   568   758
-  #  <NA>        0     0     0
-  
-  write.table(alleles, file = paste("/scratch/ahc87874/Fall2022/alleles/PhenoGeno.txt", sep = ""),
+  write.table(alleles, file = paste("/scratch/ahc87874/Fall2022/alleles/PhenoGenoComb.txt", sep = ""),
               sep = "\t", row.names = FALSE, quote = FALSE)
 } else {
-  alleles <- as_tibble(read.table("/scratch/ahc87874/Fall2022/alleles/PhenoGeno.txt", 
+  alleles <- as_tibble(read.table("/scratch/ahc87874/Fall2022/alleles/PhenoGenoComb.txt", 
                                   header = TRUE, stringsAsFactors = FALSE))
 }
 
@@ -90,12 +81,9 @@ if (FALSE) {
   alleles2 <- alleles
 
   #Range for all SNPs is 0 to 2
-  alleles2$rs62255849_T_C = round(alleles2$rs62255849_T_C, digits = 0)
-  alleles2$rs34249205_A_G = round(alleles2$rs34249205_A_G, digits = 0)
-  alleles2$rs72880701_G_T = round(alleles2$rs72880701_G_T, digits = 0)
-  alleles2$rs1817457_G_A = round(alleles2$rs1817457_G_A, digits = 0)
-  alleles2$rs149996902_CT_C = round(alleles2$rs149996902_CT_C, digits = 0)
-  alleles2$rs67393898_G_T = round(alleles2$rs67393898_G_T, digits = 0)
+  alleles2$rs80103778_G_C = round(alleles2$rs80103778_G_C, digits = 0)
+  alleles2$rs4873543_G_A = round(alleles2$rs4873543_G_A, digits = 0)
+  alleles2$rs6985833_T_G = round(alleles2$rs6985833_T_G, digits = 0)
 
   #Get minor allele %
   test <- alleles2 %>% select(starts_with("rs"))
@@ -105,37 +93,28 @@ if (FALSE) {
     print(table(test[, i]))
     print(round((sum(test[, i]) / (nrow(test) * 2)), digits = 4))
   }
-  #"rs62255849_T_C"
+  #[1] "rs80103778_G_C"
+  #rs80103778_G_C
   #    0     1     2
-  #34755  2309    42
-  #0.0322
-  #"rs34249205_A_G"
+  #69454 13238   672
+  #[1] 0.0875
+  #[1] "rs4873543_G_A"
+  #rs4873543_G_A
   #    0     1     2
-  #28419  8096   591
-  #0.125
-  #"rs72880701_G_T"
+  #45264 32296  5804
+  #[1] 0.2633
+  #[1] "rs6985833_T_G"
+  #rs6985833_T_G
   #    0     1     2
-  #31909  4985   212
-  #0.0729
-  #"rs1817457_G_A"
-  #    0     1     2
-  #18776 15201  3129
-  #0.2892
-  #"rs149996902_CT_C"
-  #    0     1     2
-  #18812 15170  3124
-  #0.2886
-  #"rs67393898_G_T"
-  #    0     1     2
-  #28544  8003   559
-  #0.1229
+  #42549 33981  6834
+  #[1] 0.2858
 
-  alleles3 <- alleles2 %>% mutate(rs62255849_T_C = ifelse(rs62255849_T_C == 0, "TT", ifelse(rs62255849_T_C == 2, "CC", "TC")),
-                                  rs34249205_A_G = ifelse(rs34249205_A_G == 0, "AA", ifelse(rs34249205_A_G == 2, "GG", "AG")),
-                                  rs72880701_G_T = ifelse(rs72880701_G_T == 0, "GG", ifelse(rs72880701_G_T == 2, "TT", "GT")),
-                                  rs1817457_G_A = ifelse(rs1817457_G_A == 0, "GG", ifelse(rs1817457_G_A == 2, "AA", "GA")),
-                                  rs149996902_CT_C = ifelse(rs149996902_CT_C == 0, "CTCT", ifelse(rs149996902_CT_C == 2, "CC", "CCT")),
-                                  rs67393898_G_T = ifelse(rs67393898_G_T == 0, "GG", ifelse(rs67393898_G_T == 2, "TT", "GT")))
+  #0 = homozygous major/ref
+  #0 = homozygous minor/alt
+  
+  alleles3 <- alleles2 %>% mutate(rs80103778_G_C = ifelse(rs80103778_G_C == 0, "GG", ifelse(rs80103778_G_C == 2, "CC", "GC")),
+                                  rs4873543_G_A = ifelse(rs4873543_G_A == 0, "GG", ifelse(rs4873543_G_A == 2, "AA", "GA")),
+                                  rs6985833_T_G = ifelse(rs6985833_T_G == 0, "TT", ifelse(rs6985833_T_G == 2, "GG", "TG")))
 
   #alleles3 %>% select(rs1817457_G_A, rs149996902_CT_C) %>% table()
   #             rs149996902_CT_C
@@ -144,10 +123,10 @@ if (FALSE) {
   #           GA    51 15000   150
   #           GG     0   117 18659
 
-  write.table(alleles3, file = paste("/scratch/ahc87874/Fall2022/alleles/PhenoGeno2.txt", sep = ""),
+  write.table(alleles3, file = paste("/scratch/ahc87874/Fall2022/alleles/PhenoGenoComp2.txt", sep = ""),
                 sep = "\t", row.names = FALSE, quote = FALSE)
 } else {
-  alleles3 <- as_tibble(read.table("/scratch/ahc87874/Fall2022/alleles/PhenoGeno2.txt", 
+  alleles3 <- as_tibble(read.table("/scratch/ahc87874/Fall2022/alleles/PhenoGenoComp2.txt", 
                                   header = TRUE, stringsAsFactors = FALSE))
 }
 #x <- c("CSRV", "w6_w3_ratio", "mmol ratio", "rs67393898", 
@@ -157,29 +136,21 @@ if (FALSE) {
 #       "SSRV", "LA_TFAP", "%", "rs149996902",
 #       "SSRV", "w3FA_TFAP", "%", "rs34249205")
 
-x <- c("Self-ID", "w6_w3_ratio_NMR", "mmol ratio", "rs67393898", "w6/w3 Ratio",
-       "Self-ID", "w6_w3_ratio_NMR", "mmol ratio", "rs62255849", "w6/w3 Ratio",
-       "Strict", "w6_w3_ratio_NMR", "mmol ratio", "rs72880701", "w6/w3 Ratio",
-       "Strict", "LA_NMR_TFAP", "%", "rs1817457", "LA %",
-       "Strict", "LA_NMR_TFAP", "%", "rs149996902", "LA %",
-       "Strict", "w3FA_NMR_TFAP", "%", "rs34249205", "w3 %")
+x <- c("DHA", "mmol/l", "rs4873543", "DHA",
+       "PUFA", "mmol/l	", "rs80103778", "PUFA",
+       "w3FA_TFAP", "%", "rs4873543", "w3 %",
+       "w3FA", "mmol/l	", "rs6985833", "w3")
 
-x <- matrix(x, ncol = 5, byrow = TRUE)
+x <- matrix(x, ncol = 4, byrow = TRUE)
 
 stderror <- function(x) sd(x)/sqrt(length(x))
 
-names(alleles3)[names(alleles3) == "CSRV"] <- "Self-ID"
-names(alleles3)[names(alleles3) == "SSRV"] <- "Strict"
+alleles3$Vegetarian <- factor(alleles3$Vegetarian, c("Veg", "NonVeg"))
+alleles3$rs80103778_G_C <- factor(alleles3$rs80103778_G_C, c("GG", "GC", "CC"))
+alleles3$rs4873543_G_A <- factor(alleles3$rs4873543_G_A, c("GG", "GA", "AA"))
+alleles3$rs6985833_T_G <- factor(alleles3$rs6985833_T_G, c("TT", "TG", "GG"))
 
-alleles3$'Self-ID' <- factor(alleles3$'Self-ID', c("Veg", "NonVeg"))
-alleles3$Strict <- factor(alleles3$Strict, c("Veg", "NonVeg"))
-alleles3$rs62255849_T_C <- factor(alleles3$rs62255849_T_C, c("TT", "TC", "CC"))
-alleles3$rs34249205_A_G <- factor(alleles3$rs34249205_A_G, c("AA", "AG", "GG"))
-alleles3$rs72880701_G_T <- factor(alleles3$rs72880701_G_T, c("GG", "GT", "TT"))
-alleles3$rs1817457_G_A <- factor(alleles3$rs1817457_G_A, c("GG", "GA", "AA"))
-alleles3$rs149996902_CT_C <- factor(alleles3$rs149996902_CT_C, c("CTCT", "CCT", "CC"))
-alleles3$rs67393898_G_T <- factor(alleles3$rs67393898_G_T, c("GG", "GT", "TT"))
-
+##########################################################################################
 for (i in 1:nrow(x)) {
   print(x[i, ])
   phenoavg <- alleles3 %>% select(contains(x[i, ]))
