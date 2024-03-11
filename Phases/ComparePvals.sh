314eced1fc54e3ba22675d62aa9676586b926b80
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-03-10 20:40:55 -0400

    Create ComparePvals.sh

---
+++ Phases/ComparePvals.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=GetAlleles
+#SBATCH --ntasks=16
+#SBATCH --nodes=1
+#SBATCH --time=144:00:00
+#SBATCH --mem=30000
+#SBATCH --output=ComparePvals.%j.out
+#SBATCH --error=ComparePvals.%j.err
+#SBATCH --mail-user=ahc87874@uga.edu
+#SBATCH --mail-type=ALL
+
+cd $SLURM_SUBMIT_DIR
+
+module load R/4.3.1-foss-2022a
+R CMD BATCH ComparePvals2.R
