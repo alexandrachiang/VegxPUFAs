986e2a907c6893847012ea88649a6503c209a6a2
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-03-11 14:50:32 -0400

    Update EffectCI.R

--- Plots/EffectCI.R
+++ Plots/EffectCI.R
@@ -12,7 +12,7 @@ for (i in exposure) {
   print("")
   print(paste("DHA", i))
   DHAData <- as_tibble(read.table(paste(GEMdir, "DHA", "DHAxVegetarian-chr8", sep = "/"), 
-                                         header = TRUE, stringsAsFactors = FALSE))
+                                         header = TRUE, stringsAsFactors = FALSE, fill = TRUE))
   temp <- DHAData %>% filter(CHR == 8, POS == 52231394) %>% mutate(Pheno = "DHA", Exposure = i) %>% select(Pheno, Phase, everything()) 
   temp %>% print(width=Inf)
   if (i == "Veg") {
@@ -24,7 +24,7 @@ for (i in exposure) {
   print("")
   print(paste("PUFA", i))
   PUFAData <- as_tibble(read.table(paste(GEMdir, "PUFA", "PUFAxVegetarian-chr2", sep = "/"), 
-                                         header = TRUE, stringsAsFactors = FALSE))
+                                         header = TRUE, stringsAsFactors = FALSE, fill = TRUE))
   temp <- PUFAData %>% filter(CHR == 2, POS == 85067224) %>% mutate(Pheno = "PUFA", Exposure = i) %>% select(Pheno, Phase, everything()) 
   temp %>% print(width=Inf)
   allData <- rbind(allData, temp)
@@ -32,7 +32,7 @@ for (i in exposure) {
   print("")
   print(paste("w3FA_TFAP", i))
   w3FA_TFAPData <- as_tibble(read.table(paste(GEMdir, "w3FA_TFAP", "w3FA_TFAPxVegetarian-chr8", sep = "/"), 
-                                         header = TRUE, stringsAsFactors = FALSE))
+                                         header = TRUE, stringsAsFactors = FALSE, fill = TRUE))
   temp <- w3FA_TFAPData %>% filter(CHR == 8, POS == 52231394) %>% mutate(Pheno = "w3FA_TFAP", Exposure = i) %>% select(Pheno, Phase, everything()) 
   temp %>% print(width=Inf)
   allData <- rbind(allData, temp)
@@ -40,7 +40,7 @@ for (i in exposure) {
   print("")
   print(paste("w3FA", i))
   w3FAData <- as_tibble(read.table(paste(GEMdir, "w3FA", "w3FAxVegetarian-chr8", sep = "/"), 
-                                         header = TRUE, stringsAsFactors = FALSE))
+                                         header = TRUE, stringsAsFactors = FALSE, fill = TRUE))
   temp <- w3FAData %>% filter(CHR == 8, POS == 52486885) %>% mutate(Pheno = "w3FA", Exposure = i) %>% select(Pheno, Phase, everything()) 
   temp %>% print(width=Inf)
   allData <- rbind(allData, temp)
