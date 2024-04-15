4885d8b294d083f0add609f7632e194234493c8f
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-04-15 14:17:03 -0400

    Create MakeBMIPheno.R

---
+++ vQTL/MakeBMIPheno.R
@@ -0,0 +1,10 @@
+#Load packages and set up wd
+suppressMessages(library(plyr))
+suppressMessages(library(dplyr))
+suppressMessages(library(data.table))
+suppressMessages(library(tidyverse))
+#suppressMessages(library(ggpubr))
+suppressMessages(library(ukbtools)) #<3
+suppressMessages(library(rio))
+
+setwd("/scratch/ahc87874/Fall2022/pheno")
