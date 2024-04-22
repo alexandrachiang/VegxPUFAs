000b6bf96d0474e28495b8f1337e757fc831dc59
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2024-04-22 02:04:20 -0400

    Update test.R

--- Plots/test.R
+++ Plots/test.R
@@ -10,7 +10,7 @@ phenos <- c("w3FA", "w3FA_TFAP", "MUFA_TFAP", "PUFA_MUFA_ratio")
 
 exposures <- c("Vegetarian")
 
-suffix <- c("comb") #, "phase1", "phase2"
+suffix <- c("combredo") #, "phase1", "phase2"
 
 for (k in suffix) {
   print(paste("suffix:", k))
@@ -21,34 +21,17 @@ for (k in suffix) {
     for (j in exposures) {
       print(paste("exposure:", j))
      
-      infileall <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/Combined/", i, "x", j, "Veg", k, ".txt", sep = ""), 
+      infileall <- as_tibble(read.table(paste("/scratch/ahc87874/Fall2022/CombinedRedo/", i, "x", j, "Veg", k, ".txt", sep = ""), 
                                           header = TRUE, stringsAsFactors = FALSE))
 	    
 	    infileall <- infileall %>% select(CHR, POS, robust_P_Value_Interaction, RSID)
 	    colnames(infileall) <- c("CHR", "BP", "P", "SNP")
 
       print("SNPs")	    
-      outdirSNPs = "/scratch/ahc87874/Fall2022/SNPs/"
-
-      write.table(infileall, paste(outdirSNPs, i, "x", j, k, "allSNPs.txt", sep = ""),
-                  row.names = FALSE, quote = FALSE)
-      
-      #Make table of sig SNPs (P < 1e-5)
-      sigSNPs <- infileall %>% filter(P <= 1e-5)
-      write.table(sigSNPs, paste(outdirSNPs, i, "x", j, k, "sigSNPs.txt", sep = ""),
-                  row.names = FALSE, quote = FALSE)
-  
-      #Make table of top 10 SNPs
-      newdata <- infileall[order(infileall$P), ]
-      newdata <- newdata[1:10, ]
-      write.table(newdata, paste(outdirSNPs, i, "x", j, k, "topSNPs.txt", sep = ""),
-                  row.names = FALSE, quote = FALSE)
-  
-      pvalue <- newdata$P[10]
-            
+     
       print("Manhattan")
       #Make manhattan plot
-      outdirman = "/scratch/ahc87874/Fall2022/manplots/"
+      outdirman = "/scratch/ahc87874/Fall2022/manplotsRedo/"
       expo = "Vegetarianism"
 	    
       maxy <- -log10(5e-08)
@@ -64,7 +47,7 @@ for (k in suffix) {
 
       print("QQ")
       #Make qq plot
-      outdirqq = "/scratch/ahc87874/Fall2022/qqplots/"
+      outdirqq = "/scratch/ahc87874/Fall2022/qqplotsRedo/"
 	    
       png(filename = paste(outdirqq, k, "/", i, "x", j, "_Veg_", k, "_qq.png", sep = ""), type = "cairo", width = 600, height = 600)
       qq(infileall$P, main = paste("Q-Q plot of ", i, " x ", j, " GWIS p-values", sep = ""))
